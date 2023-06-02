4fbbe5284fa4939d409ddde94a41d3acbdfede9c
marcuschau01 <90216251+marcuschau01@users.noreply.github.com>
2023-06-02 13:52:58 -0700

    Created a switch statement for user choices for departments

--- Main.java
+++ Main.java
@@ -2,5 +2,54 @@ public class Main {
     public static void main (String[] args) {
         System.out.println ("Hello");
     }
-    
+    //where should i put this
+    public static selectDepartment()
+    {
+        String department = "";
+        switch ()
+        {
+            case 0:
+                department = "??? Department";
+                break;
+            case 1:
+                department = "Meat Department";
+                break;
+            case 2:
+                department = "Seafood Department";
+                break;
+            case 3:
+                department = "Fruit Department";
+                break; 
+            case 4:
+                department = "Snack Department";
+                break; 
+            case 5:
+                department = "Wine and Beer Department";
+                break;
+            case 6:
+                department = "Medical Department";
+                break;
+            case 7:
+                department = "Books Department";
+                break;
+            case 8:
+                department = "";
+                break;
+            case 9:
+                department = "";
+                break;
+            case 10:
+                department = "";
+                break;
+            case 11:
+                department = "";
+                break;
+            case 12:
+                department = "";
+                break;
+            default:
+                System.out.println("YOUR FIRED!!!");
+                break;
+        }
+    }
 }
