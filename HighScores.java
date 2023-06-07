adfd73530b4365b77ae0aac17119ff4c0fa3d4bb
Mihir <85762703+mihir-code@users.noreply.github.com>
2023-06-07 17:57:36 +0000

    added leaderboard and score class

---
+++ HighScores.java
@@ -0,0 +1,33 @@
+import java.util.*;
+
+public class HighScores {
+    private ArrayList<UserScore> highScores;
+
+    public HighScores(){
+        highScores = new ArrayList<UserScore>();
+    }
+
+    public void addScore(UserScore u){
+        int i = 0;
+        for (i = 0; i <HighScores.size(); i++){
+            if (u.getScore() > highScores.get(i).getScore()){
+                break;
+            }
+        }
+        highScores.add(i,u);
+
+        if (highScores.size()> 10){
+            highScores.remove(10);
+        }
+
+    }
+    public String toString() {
+        String toString = "";
+        for (int i = 0; i <highScores.size(); i++){
+            toString += ("Rank " + (i+1) + ": " + highScores.get(i).toString() + "\n");
+        }
+        return toString;
+    }
+}
+
+
