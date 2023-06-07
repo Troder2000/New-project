3f9c3caf71213282af42e901756f5b567fcc1018
marcuschau01 <90216251+marcuschau01@users.noreply.github.com>
2023-06-07 18:07:04 +0000

    completed user score class

--- import java.util.java
+++ import java.util.java
@@ -1,5 +1,24 @@
 import java.util.*;
 
 public class UserScore{
-    
+    private String name;
+    private int score;
+
+    public UserScore(String name, int score) 
+    {
+        this.name = name;
+        this.score = score;
+    }
+    public int getScore() 
+    {
+        return score;
+    }
+    public String getName() 
+    {
+        return name;
+    }
+    public String toString() 
+    {
+        return "\"" + name + "\" with " + score + " points.";
+    }
 }
